c153ac7683a1aefb8eeeab092315bc22b4cc1c78
DENG <1784481110@qq.com>
2020-09-22 10:57:05 +0800

    海岛压缩

--- wjmhd_filecopy.py
+++ wjmhd_filecopy.py
@@ -37,22 +37,38 @@ def get_info_list(filename):
 
     return list1, list2, list3
 
+#删除文件
+def delete_folder(path):
+    try:
+        shutil.rmtree(path)
+    except Exception as e:
+        print('删除失败, %s' % e)
+    return
 
 #压缩岛文件夹为zip
-def zip_ya(fp_ls):
+def zip_ya(fp_ls, d_path):
     index = folder_id.index(fp_ls)
     folder_dir = folder_name[index]
-    file_news = folder_dir + '.zip'
+    path = os.path.join(d_path, folder_dir)
+    file_news = path + '.zip'
     
-    #理解
-    z = zipfile.ZipFile(file_news,'w',zipfile.ZIP_DEFLATED)
-    for dirpath, dirnames, filenames in os.walk(folder_dir):
-        fpath = dirpath.replace(folder_dir,'') #这一句很重要，不replace的话，就从根目录开始复制
-        fpath = fpath and fpath + os.sep or ''#实现当前文件夹以及包含的所有文件的压缩
-        for filename in filenames:
-            z.write(os.path.join(dirpath, filename),fpath+filename)
-            print ('压缩成功')
-    z.close()
+    filelist = []
+    if os.path.exists(file_news):
+        print('文件已存在')
+        return
+    for root, dirs, files in os.walk(path):
+        # 不过滤空文件夹
+        if not files and not dirs:
+            filelist.append(root)
+        for name in files:
+            filelist.append(os.path.join(root, name))
+    zf = zipfile.ZipFile(file_news, "w", zipfile.zlib.DEFLATED)
+    for tar in filelist:
+        arcname = tar[len(path):]
+        zf.write(tar,arcname)
+    zf.close()      
+    
+    return delete_folder(path)
 
 #创建海岛文件夹
 def creat_land_folder(fp_ls, d_path):
@@ -60,33 +76,22 @@ def creat_land_folder(fp_ls, d_path):
     os.mkdir(path)
     creat_folder(fp_ls, path)
 
-    #zip_ya(fp_ls)
-
-    return 
+    return zip_ya(fp_ls, d_path)
 
 #创建下级文件目录
 def creat_folder(fp_ls, d_path):
     try:
         index = 0
-
         for fp in folder_parent:
-
             if fp == fp_ls:
-                
-                
                 path = os.path.join(d_path, folder_name[index])
-                
                 os.mkdir(path)
-
                 id_ls = folder_id[index]
                 #复制文件
                 copy_file(id_ls, path)
-
                 creat_folder(id_ls, path)
                 id_ls = ''
-
             index = index + 1
-
     except Exception as e:
         print('创建文件失败：%s文件目录：%s' % (e, path))
 
@@ -94,24 +99,21 @@ def copy_file(fpf, new_file_path):
     try:
         index_1 = 0
         for fpf_ls in file_parent_folder:
-
-            if fpf == fpf_ls:
-                
+            if fpf == fpf_ls:               
                 old_file_path = os.path.join(files_path, file_name_old[index_1])
                 
-                if file_name_old[index_1] in list_file_old:
+                #if file_name_old[index_1] in list_file_old:
 
-                    shutil.copy(old_file_path, new_file_path)
+                shutil.copy(old_file_path, new_file_path)
+                src_file = os.path.join(new_file_path, file_name_old[index_1])
+                dst_file = os.path.join(new_file_path, file_name[index_1])
 
-                    src_file = os.path.join(new_file_path, file_name_old[index_1])
-                    dst_file = os.path.join(new_file_path, file_name[index_1])
-
-                    os.rename(src_file, dst_file)
+                os.rename(src_file, dst_file)
 
             index_1 = index_1 + 1
 
     except Exception as e:
-        print('复制文件失败：%s文件名称：%s' % (e, old_file_path))
+        print('复制文件失败：%s' % (e))
 
 #获取目录下所有文件（目录下没有文件夹）
 def get_all_file(path, list_name):
